1a92c77509c43f0fba0b3a81421cf3974ff3b8e1
Vvasco1 <109784466+Vvasco1@users.noreply.github.com>
2023-10-23 16:00:55 +0900

    Update vpc.tf

--- network/vpc.tf
+++ network/vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "dev" {
-  cidr_block           = "10.10.0.0/16" # 임의 지정
+  cidr_block           = "10.0.0.0/16" # 임의 지정
   instance_tenancy     = "default" # 인스턴스에 대한 테넌트 옵션 기본값
   tags = {
     Name = "DEV-VPC"
